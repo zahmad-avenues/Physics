f74a2b8b700a9f4c21bb9d8bc5ab1e4c3f795fc4
zahmad-avenues <zidan_ahmad@avenues-ny.org>
2025-04-14 19:15:26 -0400

    Add files via upload
    Added applyForce() function

--- sketch.js
+++ sketch.js
@@ -1,107 +1,89 @@
+let b;
+let c;
+let redVal;
+let vel;
+let pos;
+let grav;
 
-let b 
-let c
-let redVal
-let vel
-let pos
-let grav
 function setup() {
   createCanvas(400, 400);
-  grav = createVector(0,1)
-  b = new Bouncer(100,100,10,0,5);
-  
-  ellipseMode(RADIUS)
+  grav = createVector(0, -0.2);
+  b = new Bouncer(100, 100, 10, 0, 20, 10);
+  ellipseMode(RADIUS);
 }
-
 
 function draw() {
   background(220);
-  
-    b.update()    
-  
+  b.update();    
 }
 
 // a blueprint for creating circles that move around the screen
-class Bouncer { // noun
-  // properties of a bouncer (adjectives)
-  //   where it is (location)
-  //   color
-  //   positional constraints
-  //   size
-  //   velocity
-  constructor(x,y,r,redVal,velX,velY) {
-   // this.x = x
- //   this.y = y
-    this.pos = createVector(x,y)
-    this.vel = createVector(velX,velY)
-    this.r = r
-    this.redVal = redVal
-    this.c = color(redVal,200,200)
-    this.minX = r
-    this.maxX = width-r
-    this.minY = r
-    this.maxY = height-r
+class Bouncer {
+  constructor(x, y, r, redVal, velX, velY) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(velX, velY);
+    this.r = r;
+    this.redVal = redVal;
+    this.c = color(redVal, 200, 200);
+    this.minX = r;
+    this.maxX = width - r;
+    this.minY = r;
+    this.maxY = height - r;
   }
-  // behaviors of a bouncer (verbs)
-  //   draw
-  //   move
-  setConstraints(minX,maxX,minY,maxY) {
-    this.minX = minX + this.r
-    this.maxX = maxX - this.r
-    this.minY = minY + this.r 
-    this.maxY = maxY - this.r
+
+  setConstraints(minX, maxX, minY, maxY) {
+    this.minX = minX + this.r;
+    this.maxX = maxX - this.r;
+    this.minY = minY + this.r;
+    this.maxY = maxY - this.r;
   }
-  
+
+  applyForce(force) {
+    this.vel.add(force);
+  }
+
   applyVelocity() {
-   // this.dx += this.v
-    this.pos.add(this.vel)  
+    this.pos.add(this.vel);
   }
-  
-  applyGravity() {
-   // this.dy += G
-    this.vel.add(grav)
-  }
-  
+
   update() {
-    this.applyGravity()
-    this.applyVelocity()
-    this.move()
-    this.draw()
+    this.applyForce(grav);
+    this.applyVelocity();
+    this.move();
+    this.draw();
   }
-  
+
   move() {
-   // this.x += this.dx
-  //  this.y += this.dy
-    this.pos.add(this.vel)
-    if( this.pos.x < this.minX ) {
-      this.pos.x = this.minX
-      this.vel.x *= -1
-      this.changeColor()
+    if (this.pos.x < this.minX) {
+      this.pos.x = this.minX;
+      this.vel.x *= -1;
+      this.changeColor();
     }
-    if( this.pos.x > this.maxX ) {
-      this.pos.x = this.maxX
-      this.vel.x *= -1
-      this.changeColor()
+    if (this.pos.x > this.maxX) {
+      this.pos.x = this.maxX;
+      this.vel.x *= -1;
+      this.changeColor();
     }    
-    if( this.pos.y < this.minY ) {
-      this.pos.y = this.minY
-      this.vel.y *= -1
-      this.changeColor()
+    if (this.pos.y < this.minY) {
+      this.pos.y = this.minY;
+      this.vel.y *= -1;
+      this.changeColor();
     }    
-    if( this.pos.y > this.maxY ) {
-      this.pos.y = this.maxY
-      this.vel.y *= -1
-      this.changeColor()
+    if (this.pos.y > this.maxY) {
+      this.pos.y = this.maxY;
+      this.vel.y *= -1;
+      this.changeColor();
     }       
   }
+
   changeColor() {
-    console.log("color change")
-    this.redVal+=10
-    this.c = color(this.redVal,200,200)
-    
+    console.log("color change");
+    this.redVal += 10;
+    this.c = color(this.redVal, 200, 200);
   }
+
   draw() {
-    fill(this.c)
-    circle(this.pos.x,this.pos.y,this.r)
+    fill(this.c);
+    circle(this.pos.x, this.pos.y, this.r);
   }
 }
